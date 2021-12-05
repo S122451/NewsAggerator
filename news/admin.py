ab936a00757b4aacc784512815877984b3f1af6e
Wurud Salih <s122451@ap.be>
2021-12-05 14:31:19 +0100

    Haalt artikelen op

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Episode
+from .models import Article
 
-@admin.register(Episode)
+@admin.register(Article)
 class EpisodeAdmin(admin.ModelAdmin):
-    list_display = ("podcast_name", "title", "pub_date")
+    list_display = ("title","image")
